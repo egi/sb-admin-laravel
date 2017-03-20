31b13e1bda28468d3f8e08da604d2278bbed87cd
Agastiya S. Mohammad <egi@pure-technology.net>
2017-03-20 08:47:02 +0700

    lazy chart definition, to minimize err

--- app/assets/scripts/frontend.js
+++ app/assets/scripts/frontend.js
@@ -1,85 +1,92 @@
+// app/assets/scripts/frontend.js
 
-var lineChartData = {
-    labels : ["January","February","March","April","May","June","July"],
-    datasets : [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill : "#fff",
-            pointHighlightStroke : "rgba(6, 197, 172, 1)",
-            data : [65,59,80,81,56,55,40]
-        },
-        {
-            label: "My Second dataset",
-             fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill : "#fff",
-            pointHighlightStroke : "rgba(244, 204, 11, 1)",
-            data : [28,48,40,19,86,27,90]
-        }
-    ]
+var cline = document.getElementById("cline");
+if (cline) {
+    var lineChartData = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(6, 197, 172, 1)",
+                data : [65,59,80,81,56,55,40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(244, 204, 11, 1)",
+                data : [28,48,40,19,86,27,90]
+            }
+        ]
+    };
 
-}
-
-
-    var cline = document.getElementById("cline").getContext("2d");
-    new Chart(cline).Line(lineChartData, {
+    new Chart(cline.getContext("2d")).Line(lineChartData, {
         responsive: true
     });
-   
+}
 
-   var pdata = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-       color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
-]
-var cpie = document.getElementById("cpie").getContext("2d");
-new Chart(cpie).Pie(pdata, { responsive: true});
+var cpie = document.getElementById("cpie");
+if (cpie) {
+    var pdata = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
 
-var ddata = [
-    {
-        value: 50,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 300,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 160,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
-]
-var cdonut = document.getElementById("cdonut").getContext("2d");
-new Chart(cdonut).Doughnut(ddata, { responsive: true});
+    new Chart(cpie.getContext("2d")).Pie(pdata, { responsive: true});
+}
 
-var bdata = {
+var cdonut = document.getElementById("cdonut");
+if (cdonut) {
+    var ddata = [
+        {
+            value: 50,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 300,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 160,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+    new Chart(cdonut.getContext("2d")).Doughnut(ddata, { responsive: true});
+}
+
+var cbar = document.getElementById("cbar");
+if (cbar) {
+    var bdata = {
         labels : ["January","February","March","April","May","June","July"],
         datasets : [
             {
@@ -90,53 +97,53 @@ var bdata = {
                 data : [130,160,95,205,170,135,200]
             },
             {
-               fillColor: "rgba(151,187,205,0.5)",
+                fillColor: "rgba(151,187,205,0.5)",
                 strokeColor: "rgba(151,187,205,0.8)",
                 highlightFill: "rgba(151,187,205,0.75)",
                 highlightStroke: "rgba(151,187,205,1)",
                 data : [85,90,160,145,180,130,195]
             }
         ]
+    };
+    new Chart(cbar.getContext("2d")).Bar(bdata, {
+        responsive : true
+    });
+}
 
-    }
-    var cbar = document.getElementById("cbar").getContext("2d");
-    new Chart(cbar).Bar(bdata, {
-            responsive : true
-        });
-
+var cpolar = document.getElementById("cpolar");
+if (cpolar) {
     var podata = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    },
-    {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-    },
-    {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
-    }
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
 
-]
-
-var cpolar = document.getElementById("cpolar").getContext("2d");
-new Chart(cpolar).PolarArea(podata, { responsive: true});
+    ];
+    new Chart(cpolar.getContext("2d")).PolarArea(podata, { responsive: true});
+}
